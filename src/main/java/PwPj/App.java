86033dc565008624ddd09bf3c0f9fe9552c61af9
swag31415 <kidswagatam18@gmail.com>
2019-08-27 17:01:43 -0600

    Added JavaFX

--- src/main/java/PwPj/App.java
+++ src/main/java/PwPj/App.java
@@ -2,10 +2,13 @@ package PwPj;
 
 import java.util.Scanner;
 
-public class App {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
 
     public static void main(String[] args) {
-        
+        launch(args);
     }
 
     public String getInput() {
